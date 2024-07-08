ee922a7b8e357faeaf3bcd9307abaa28ad224d29
ANKIT KUMAR VERMA <vermaankit.ak555@gmail.com>
2024-07-08 20:24:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// let count=0;
+//const[count,setCount]=useState(count);
+// function decreasehandler()
+// {
+//   setCount(count-1);
 
+// }
 function App() {
+const[count,setCount]=useState(0);
+function decreasehandler()
+{
+  setCount(count-1);
+
+}
+function increasehandler()
+{
+  setCount(count+1);
+}
+function restehandler()
+{
+  setCount(0);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <button id='add' onClick={increasehandler}>+
+    </button>
+    <span>{count}</span>
+    <button id='add' onClick={decreasehandler}>-
+    </button>
+    <button id="reset" onClick={restehandler}>reset</button>
+
     </div>
   );
 }
